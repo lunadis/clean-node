d2e749e1ba32de2f675d185e165d62f73a22b6c9
Luis Lopes <luis@mecanizou.com>
2023-06-13 18:33:17 -0300

    init clean node template

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-/**
- * oi thaisa
- */
-/**
- * oi luis
- */
+const app = require('express')()
+
+app.get('/', (req, res) => {
+  res.status(200).send('ahoy');
+})
+
+app.listen(3333, () => {
+  console.log('its live!')
+})
